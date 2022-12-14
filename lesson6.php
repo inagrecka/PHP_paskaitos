26a7a4d8bb875ccbc31d441cb84551175d4e6ab3
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2022-12-14 19:41:49 +0200

    initial commit

---
+++ lesson6.php
@@ -0,0 +1,16 @@
+<?php
+
+$masyvas = ['Ina', 'Giedrius', 'Ausra'];
+
+$masyvas1 = []; //-tuscias masyvas
+
+var_dump($masyvas);
+
+echo $masyvas[2];
+
+//masyvu pridejimas
+$masyvas2 = [];
+$masyvas2[] = 25;
+$masyvas2['num_1'] = 102;
+var_dump($masyvas2);
+
