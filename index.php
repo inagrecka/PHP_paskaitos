e0d0b6ceba60c356046b228905a7cde910c6a2b8
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2023-01-04 20:31:43 +0200

    tema PHP SERVER 2 uzd

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
-<?php
-
-echo "test";
-echo "labas";
-
-echo "testas";
-
-
-echo "bandome";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php for ($i = 0; $i <= 10; $i++): //uztenka <?php nurodyti pradzioje
+    if ($i % 2 === 0):
+       echo  '<strong>'.$i.'</strong>'.'<br>';
+    else:
+        echo $i.'<br>';
+    endif;
+endfor; ?> <!-- ir gale php uzdaryti su ?> -->
+</body>
+</html>
