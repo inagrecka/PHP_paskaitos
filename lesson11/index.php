8680a4e5b4afed8176e7d030a57c630bf7fc7c35
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2023-01-09 17:45:21 +0200

    4 uzd sprendimas visas

--- lesson11/index.php
+++ lesson11/index.php
@@ -8,11 +8,32 @@
     <title>Document</title>
 </head>
 <body>
-New TODO
 <form method="POST" action="submit.php">
-    <input type="text" name="todo"/>
+    <label for="todoInput"> New TODO list </label>
+    <br>
+    <input type="text" id="todoInput" name="todo" placeholder="ToDo"/>
     <input type="submit">
 </form>
+
+<!--
+DARBO EIGA:
+1. nuskaitome JSON failą;
+2. konvertuojam į MASYVĄ;
+3. paleidžiam FOREACH ciklą
+-->
+
+<?php
+$fileItems = file_get_contents('new.json');
+$todoList = json_decode($fileItems, true);
+?>
+
+<h1> ToDo list: </h1>
+<ul>
+    <?php foreach ($todoList as $item): ?>
+    <li> <?= $item['todo'] ?> </li>
+    <?php endforeach ?>
+</ul>
 </body>
 </html>
+
 
--- lesson11/submit.php
+++ lesson11/submit.php
@@ -1,1 +1,30 @@
 <?php
+$oldTodo = file_get_contents('new.json');
+//var_dump($oldTodo);
+$deserializedOldTodo = json_decode($oldTodo, true);
+//var_dump($deserializedOldTodo);
+
+$deserializedOldTodo[] = $_POST['todo'];
+/*jeigu input'ui priskirsim kintamaji, tada i masyva idesime kintamaji (be POST)
+ $new = $_POST['todo'];
+ $deserializedOldTodo[] = $new; */
+
+$serializedOldTodo = json_encode($deserializedOldTodo, JSON_PRETTY_PRINT);
+$newTodo = file_put_contents('new.json', $serializedOldTodo);
+?>
+
+<h3>Task submitted: <?= $_POST['todo'] ?></h3>
+<button onclick="history.go(-1)"> BACK </button>
+<a href="/">GO BACK</a> <!-- kitas variantas mygtuko "ATGAL" -->
+
+<!--
+DARBO EIGA:
+1. sukuriam naują JSON failą;
+2. paimam duomenis iš JSON failo;
+3. juos konvertuojam į MASYVĄ;
+4. atliekame reikalingus veiksmus su esamu MASYVU;
+5. naują MASYVĄ konvertuojam į JSON failą;
+6. MASYVO duomenis įdedame į JSON failą.
+-->
+
+
