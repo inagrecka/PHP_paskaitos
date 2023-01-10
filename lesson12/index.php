562d795ad80b170cfe4ecb98e41a507a09eb0ae2
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2023-01-10 21:43:54 +0200

    3 uzd sprendimas

--- lesson12/index.php
+++ lesson12/index.php
@@ -21,16 +21,16 @@ declare(strict_types=1);
 "+3706234567a" - false
 */
 
-function task2(string $number):bool {
-    $pattern = '/^\+(3706)\d{7}$/';
-    $result = preg_match($pattern, $number);
-    if ($result !== 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-var_dump(task2('4454tgsegsd'));
+//function task2(string $number):bool {
+//    $pattern = '/^\+(3706)\d{7}$/';
+//    $result = preg_match($pattern, $number);
+//    if ($result !== 0) {
+//        return true;
+//    } else {
+//        return false;
+//    }
+//}
+//var_dump(task2('4454tgsegsd'));
 /*
 3. Patobulinkite funkciją (2). Funkcija turėtų galėti patikrinti ir tokius telefono numerius:
 "+370 623 45678"
@@ -39,9 +39,27 @@ var_dump(task2('4454tgsegsd'));
 "00370 623 45678"
 Jeigu telefono numeris validus, iš funkcijos turėtų grįžti tvarkingai suformatuotas telefono numeris:
 "+370-623 45678" --> "+37062345678"
+*/
+
+function task3(string $number):string {
+    $pattern = '/^(\+|0{2})(370)( |\-)(6)(\d{2})( |\-)(\d{5})$/';
+    $replacement = '+$2$4$5$7'; //pazymime tik regex'o grupes, kurias norime isspausdinti (naudojame dolerio $ zenkla)
+    $result = preg_match($pattern, $number);
+    if ($result !== 0) {
+        return preg_replace($pattern, $replacement, $number);
+    } else {
+        return 'Number not valid';
+    }
+}
+var_dump(task3('00370 623 45678'));
+
+/*
 4. Parašykite funkciją, kuri užmaskuotų dalį vartotojo duomenų. Pavardės ir gimimo metų simboliai
 turėtų būti pakeisti i simbolius 'X'.
 "John Smith, 1979 05 15" --> "John XXXXX, XXXX 05 15"
+*/
+
+/*
 5. Parašykite funkciją, kuri pravaliduotų IPv4 adresą. IPv4 adresas yra sudarytas iš 4 skaičių, kurių kiekvienas gali
 būti nuo 0 iki 255. Skaičiai atskirti taškais.
 Pvz.:
